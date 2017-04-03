aa41114f81fbfe5cbf46771a1ed8b870b50fc3e7
Anthony <aliang8@stuy.edu>
2017-04-02 20:49:18 -0400

    added params

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv,json,os 
 from utils import energy
 
 app = Flask(__name__)
 data = energy.get_reports()
-schema = energy._test_interfaces()
-print(schema)
+#schema = energy._test_interfaces()
+#print(schema)
 
 @app.route("/", methods=['POST','GET'])
 def home():
@@ -15,9 +15,11 @@ def home():
 def map():
     return render_template('map.html')
 
-@app.route("/ports")
+@app.route("/ports", methods=['POST','GET'])
 def ports():
-    return render_template('ports.html',data=data)
+    year = request.args.get('year')
+    category = str(request.args.get('category'))
+    return render_template('ports.html',data=data,year=year,category=category)
 
 if __name__ == '__main__': 
     app.debug = True
